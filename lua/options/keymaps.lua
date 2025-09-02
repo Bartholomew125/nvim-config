88eab1f33ccbc49f5bf276e64f752227ee9329d3
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-09-02 09:34:15 +0200

    Added keymaps to switch windows

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -26,3 +26,9 @@ vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", { desc = "Go 
 
 -- Terminal
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
+
+-- Window switching
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to down window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to up window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
