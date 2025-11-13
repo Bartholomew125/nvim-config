dd4c87d70abda2b83486a4e11053be0c50fe7071
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-11-13 13:18:48 +0100

    Fixed indents

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,29 +2,29 @@ return {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for Neovim
-    -- Mason must be loaded before its dependents so we need to set it up here.
-    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-    { 
-        'mason-org/mason.nvim', 
-        opts = {} 
-    },
+        -- Automatically install LSPs and related tools to stdpath for Neovim
+        -- Mason must be loaded before its dependents so we need to set it up here.
+        -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+        { 
+            'mason-org/mason.nvim', 
+            opts = {} 
+        },
 
-    'mason-org/mason-lspconfig.nvim',
+        'mason-org/mason-lspconfig.nvim',
 
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
 
-    -- Useful status updates for LSP.
-    { 
-        'j-hui/fidget.nvim', 
-        opts = {} 
-    },
+        -- Useful status updates for LSP.
+        { 
+            'j-hui/fidget.nvim', 
+            opts = {} 
+        },
     },
 
     config = function()
-    require('mason-lspconfig').setup {
-        ensure_installed = { "jdtls" }, 
-        automatic_installation = false
-    }
+        require('mason-lspconfig').setup {
+            ensure_installed = { "jdtls" }, 
+            automatic_installation = false
+        }
     end,
 }
